0dd88c310203d8d789de5f1bcfa6897671d63120
Noyer Romain <romainnoyer1995@gmail.com>
2023-02-15 17:22:32 +0100

    Mentorat 15/02

--- back-end/models/sauce.js
+++ back-end/models/sauce.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const sauceSchema = mongoose.Schema({
+  userId: { type: String },
   name: { type: String, required: true },
   manufacturer: { type: String, required: true },
   description: { type: String, required: true },
@@ -9,9 +10,8 @@ const sauceSchema = mongoose.Schema({
   heat: { type: Number },
   likes: { type: Number, default: 0 },
   dislikes: { type: Number, default: 0 },
-  userId: { type: String },
-  usersLiked: { type: [String], required: true },
-  usersDisliked: { type: [String], required: true },
+  usersLiked: [String],
+  usersDisliked: [String],
 });
 
 module.exports = mongoose.model("Sauce", sauceSchema);
